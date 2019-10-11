1c042496e7e64db1f0a582316b4c9e6c5529a156
Isaac Mbuotidem <Mbuotidem.Isaac@hennepin.us>
2019-10-11 17:59:45 -0500

    Fixing url's.

--- ClientApp/src/app/api.service.ts
+++ ClientApp/src/app/api.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { environment } from '../environments/environment';
+
 
 @Injectable()
 export class ApiService {
+  baseUrl = environment.baseUrl;
+
   private selectedQuestion = new Subject<any>();
   questionSelected = this.selectedQuestion.asObservable();
 
@@ -13,40 +17,51 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getQuestions(quizId) {
-    return this.http.get(`https://localhost:44353/api/questions/${quizId}`);
-
+    return this.http.get(`/api/questions/${quizId}`);
+    //`${this.baseUrl}/api/questions/${quizId}`
+    //`https://localhost:44353/api/questions/${quizId}`
   }
 
   getQuizzes() {
-    return this.http.get('https://localhost:44353/api/quizzes');
-
+    return this.http.get(`/api/quizzes`);
+    //`${this.baseUrl}/api/quizzes`
+    //'https://localhost:44353/api/quizzes'
   }
 
   getAllQuizzes() {
-    return this.http.get('https://localhost:44353/api/quizzes/all');
-
+    return this.http.get(`/api/quizzes/all`);
+    //`${this.baseUrl}/api/quizzes/all`
+    //'https://localhost:44353/api/quizzes/all'
   }
   postQuestion(question) {
-    this.http.post('https://localhost:44353/api/questions', question).subscribe(res => {
+    this.http.post(`/api/questions`, question).subscribe(res => {
+      //`${this.baseUrl}/api/questions`
+      //'https://localhost:44353/api/questions'
       console.log(res);
     });
 
   }
 
   putQuestion(question) {
-    this.http.put(`https://localhost:44353/api/questions/${question.id}`, question).subscribe(res => {
+    this.http.put(`/api/questions/${question.id}`, question).subscribe(res => {
+//`${this.baseUrl}/api/questions/${question.id}`
+//`https://localhost:44353/api/questions/${question.id}`
       console.log(res);
     });
   }
 
   postQuiz(quiz) {
-    this.http.post('https://localhost:44353/api/quizzes', quiz).subscribe(res => {
+    this.http.post(`/api/quizzes`, quiz).subscribe(res => {
+      //`${this.baseUrl}/api/quizzes`
+      //'https://localhost:44353/api/quizzes'
       console.log(res);
     });
   }
 
   putQuiz(quiz) {
-    this.http.put(`https://localhost:44353/api/quizzes/${quiz.id}`, quiz).subscribe(res => {
+    this.http.put(`/api/quizzes/${quiz.id}`, quiz).subscribe(res => {
+      //`${this.baseUrl}/api/quizzes/${quiz.id}`
+      //`https://localhost:44353/api/quizzes/${quiz.id}`
       console.log(res);
     });
   }
--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://interro.azurewebsites.net'
 };
